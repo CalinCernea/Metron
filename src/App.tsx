42dca855607fcef7d3ef00cd71b4cc7de8f43c77
NutriStat <calincernea803@yahoo.com>
2025-10-18 13:07:30 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-slate-900 min-h-screen flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">
+        Metron - Build in Progress
+      </h1>
+    </div>
+  )
+}
+export default App
