e277dbfe91afde77d6fcecc72833ea58c23f85b5
CalinCernea <calincernea803@yahoo.com>
2025-10-23 16:18:00 +0300

    Refactor Vite config to limit env variable exposure
    Removed loading of all environment variables and adjusted configuration to expose only specific prefixes.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode } ) => {
-  const env = loadEnv(mode, process.cwd(), 
-  // Load all env vars (not just those prefixed with VITE_)
-  '') 
-
-  return {
-    plugins: [react()],
-    define: {
-      // Expose env vars to your client code
-      // Make sure to stringify them, otherwise Vite will replace them with their raw values
-      // which can be problematic for security and type safety
-      'process.env': JSON.stringify(env),
-    },
-  }
+export default defineConfig({
+  plugins: [react( )],
+  // Adaugă această secțiune pentru a expune variabilele de mediu
+  envPrefix: ["VITE_", "SUPABASE_"],
 })
