fb6a81b97bc11d808271a25439835b29d1ef0cd8
CalinCernea <calincernea803@yahoo.com>
2025-10-23 16:01:05 +0300

    Refactor vite.config.ts to load env variables
    Updated Vite configuration to load environment variables.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({ mode } ) => {
+  const env = loadEnv(mode, process.cwd(), 
+  // Load all env vars (not just those prefixed with VITE_)
+  '') 
+
+  return {
+    plugins: [react()],
+    define: {
+      // Expose env vars to your client code
+      // Make sure to stringify them, otherwise Vite will replace them with their raw values
+      // which can be problematic for security and type safety
+      'process.env': JSON.stringify(env),
+    },
+  }
 })
